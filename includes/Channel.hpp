d0cca5a295d34e4f56b5eae454ba95c64a9d9eb5
Edwin ANNE <edwin_anne@outlook.fr>
2025-07-23 13:14:33 +0200

    + | Ajout dans la classs Channel des operateurs pour les droits users sur le channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: Edwin ANNE <eanne@student.42lehavre.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 21:03:27 by Edwin ANNE        #+#    #+#             */
-/*   Updated: 2025/07/15 11:54:30 by Edwin ANNE       ###   ########.fr       */
+/*   Updated: 2025/07/23 13:13:12 by Edwin ANNE       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ class Channel
 		std::string name; // Nom du channel
 		std::string topic; // Sujet du channel
 		std::vector<int> clients; // Liste des descripteurs de fichiers des clients connectés au channel
+		std::vector<int> operators; // Liste des opérateurs du channel
 		
 	public:
 		Channel(const std::string& channelName) : name(channelName) {}
@@ -41,6 +42,12 @@ class Channel
 		void RemoveClient(int fd);
 		bool HasClient(int fd) const;
 		size_t GetClientCount() const { return clients.size(); }
+		
+		// Gestion Opérateurs
+		void AddOperator(int fd);
+		void RemoveOperator(int fd);
+		bool IsOperator(int fd) const;
+		std::vector<int> GetOperators() const { return operators; }
 };
 
 #endif
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: Edwin ANNE <eanne@student.42lehavre.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 21:03:43 by Edwin ANNE        #+#    #+#             */
-/*   Updated: 2025/07/13 21:04:42 by Edwin ANNE       ###   ########.fr       */
+/*   Updated: 2025/07/23 13:13:13 by Edwin ANNE       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,28 @@ void Channel::RemoveClient(int fd) {
 bool Channel::HasClient(int fd) const {
 	for (size_t i = 0; i < clients.size(); i++) {
 		if (clients[i] == fd)
+			return (true);
+	}
+	return (false);
+}
+
+void Channel::AddOperator(int fd) {
+	if (!IsOperator(fd))
+		operators.push_back(fd);
+}
+
+void Channel::RemoveOperator(int fd) {
+	for (size_t i = 0; i < operators.size(); i++) {
+		if (operators[i] == fd) {
+			operators.erase(operators.begin() + i);
+			break;
+		}
+	}
+}
+
+bool Channel::IsOperator(int fd) const {
+	for (size_t i = 0; i < operators.size(); i++) {
+		if (operators[i] == fd)
 			return (true);
 	}
 	return (false);
